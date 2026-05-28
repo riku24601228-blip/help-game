b278c6a3ea1c66c4f987a353221bc5c4c63a07b0
riku <riku24601228@gmail.com>
2026-05-28 20:59:15 +0900

    オリジナル作品１

---
+++ Assets/Forever_MoveH.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Forever_MoveH : MonoBehaviour
+{
+    public float speed = 1;
+
+    void FixedUpdate()
+    {
+        this.transform.Translate(speed / 50, 0, 0);
+    }
+}
+
